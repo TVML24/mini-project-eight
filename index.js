dfd5e5cab169e072866a5f9b8f7125d38c376165
Tim <tvanmookletcher@gmail.com>
2023-02-19 12:21:24 +1030

    all done in one

---
+++ index.js
@@ -0,0 +1,9 @@
+const { default: inquirer } = require("inquirer");
+const Game = require("./lib/Game");
+
+// Initialize a new Game object
+const game = new Game();
+
+// Start playing
+game.play();
+
